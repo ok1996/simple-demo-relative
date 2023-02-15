4eeff4c97fe98cb2e2d6900f83519812140f65d2
ok1996 <ku29@qq.com>
2023-02-15 17:11:54 +0800

    Initial demo project

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # simple-demo-relative
 Simple独立示例模块
+
+## demo-native示例模块
+| 模块名称             | 说明                         | 文档                    |
+|------------------|----------------------------|-----------------------|
+| native-demo-boot | native示例simple-starter-web | demo-native\README.md |
+
---
+++ demo-native/native-demo-boot/src/main/java/cn/iosd/demo/boot/BootApplication.java
@@ -0,0 +1,14 @@
+package cn.iosd.demo.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author ok1996
+ */
+@SpringBootApplication
+public class BootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BootApplication.class, args);
+    }
+}
