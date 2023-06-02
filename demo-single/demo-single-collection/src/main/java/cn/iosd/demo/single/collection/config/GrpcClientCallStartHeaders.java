a627c3863f7597e8ebc96e3eb57b3899769037af
ok1996 <ku29@qq.com>
2023-06-02 15:01:25 +0800

    version:2023.3.4.0

---
+++ demo-single/demo-single-collection/src/main/java/cn/iosd/demo/single/collection/config/GrpcClientCallStartHeaders.java
@@ -0,0 +1,19 @@
+package cn.iosd.demo.single.collection.config;
+
+
+import cn.iosd.starter.grpc.client.interceptor.ClientCallStartHeaders;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * @author ok1996
+ */
+@Component
+public class GrpcClientCallStartHeaders implements ClientCallStartHeaders {
+    @Override
+    public Map<String, String> headers() {
+        return Collections.singletonMap("token", "ces1");
+    }
+}
