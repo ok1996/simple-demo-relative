7806be112803f174ca0729567381f4fc28a1eb66
ok1996 <ku29@qq.com>
2023-07-04 15:27:32 +0800

    version: 2023.4.0.0

--- demo-single/demo-single-collection/src/main/java/cn/iosd/demo/single/collection/config/CollectionAutoConfig.java
+++ demo-single/demo-single-collection/src/main/java/cn/iosd/demo/single/collection/config/CollectionAutoConfig.java
@@ -1,7 +1,11 @@
 package cn.iosd.demo.single.collection.config;
 
+import org.flywaydb.core.Flyway;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * <p>
@@ -13,4 +17,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @MapperScan("cn.iosd.demo.single.collection.mapper")
 public class CollectionAutoConfig {
+    @Bean
+    public Flyway collectionFlyway(DataSource dataSource) {
+        Flyway flyway = Flyway.configure()
+                .dataSource(dataSource)
+                .locations("classpath:db/mysql/collection")
+                .baselineOnMigrate(true)
+                .table("collection_flyway_schema_history")
+                .load();
+        flyway.migrate();
+        return flyway;
+    }
 }
